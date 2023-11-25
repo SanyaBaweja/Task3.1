51321bc9e79cbb756f4b2109e0cfdb6b100aa5ec
SanyaBaweja <bawejasanya2602@gmail.com>
2023-11-25 15:09:19 +1100

    Initial commit

---
+++ Task3.1/EmptyClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Clock
+{
+    public class Clock
+    {
+        Counter _sec = new("Seconds");
+        Counter _min = new("Minutes");
+        Counter _hrs = new("Hours");
+
+        public void IncrementClock()
+        {
+            _sec.Increment();
+            if (_sec.Ticks == 60)
+            {
+                _sec.Reset();
+                _min.Increment();
+
+                if (_min.Ticks == 60)
+                {
+                    _min.Reset();
+                    _hrs.Increment();
+
+                    if (_hrs.Ticks == 24)
+                    {
+                        _hrs.Reset();
+
+                    }
+                }
+            }
+        }
+
+
+        public string Output()
+        {
+            return $"{_hrs.Ticks:00} : {_min.Ticks:00} : {_sec.Ticks:00}";
+        }
+
+        //      public void ResetClock()
+        //{
+        //	_hrs.Reset();
+        //	_min.Reset();
+        //	_sec.Reset();
+        //}
+    }
+}
---
+++ Task3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Clock
+{
+    class Clock1
+    {
+        static void Main()
+        {
+            Clock clock1 = new();  //new Instance
+
+            for (int i = 0; i < 86400; i++)  // 86400 are the no of seconds in 24 hrs so the increment will go till no of seconds reach 86400
+            {
+                clock1.IncrementClock();
+                Console.WriteLine(clock1.Output());
+            }
+            //clock1.ResetClock();
+            Console.ReadLine();
+        }
+
+    }
+}
